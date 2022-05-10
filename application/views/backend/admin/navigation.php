cdb1b490c089eda0c53b889ed3d5e0d1a62b2eba
A-K-A-S-H-git <70967776+A-K-A-S-H-git@users.noreply.github.com>
2022-05-11 01:42:18 +0530

    changes

--- application/views/backend/admin/navigation.php
+++ application/views/backend/admin/navigation.php
@@ -8,7 +8,7 @@
                             <input type="text" class="form-control" placeholder="Search..."> <span class="input-group-btn">
             <button class="btn btn-default" type="button"> <i class="fa fa-search"></i> </button>
             </span> </div>
-                        <!-- /input-group -->
+            
                     </li>
                     <li class="user-pro">
 
